c5c810aa4a6dd4d7e282fe8fefdee07296de6380
pjhernandez18 <pjhernan@usc.edu>
2019-01-06 23:00:43 -0800

    added node.js file

---
+++ node.js/yelp-fusion.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const yelp = require('yelp-fusion');
+
+const client = yelp.client('ezVut3lGqFJ7HXBTot9_Y8Nv2lcAwbVucOjwhk-g2iAwiyG5gnZ0NBm9U0D7X5bxAhRI4RhJ9DHVfg37r_32BRvg2Ys0jHSZvPoW5IymJq3DyPI9bVzz8Oa3lEkxXHYx');
+
+client.search({
+  term:'Chinese',
+  location: 'los angeles'
+}).then(response => {
+  console.log(response.jsonBody.businesses);
+}).catch(e => {
+  console.log(e);
+});
