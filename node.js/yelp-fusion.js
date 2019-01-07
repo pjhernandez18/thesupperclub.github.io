2a6842ad98f7221c2e4534235823f2d71f72e676
pjhernandez18 <pjhernan@usc.edu>
2019-01-06 23:37:02 -0800

    need to use express and node.js to make this work

--- node.js/yelp-fusion.js
+++ node.js/yelp-fusion.js
@@ -5,8 +5,8 @@ const yelp = require('yelp-fusion');
 const client = yelp.client('ezVut3lGqFJ7HXBTot9_Y8Nv2lcAwbVucOjwhk-g2iAwiyG5gnZ0NBm9U0D7X5bxAhRI4RhJ9DHVfg37r_32BRvg2Ys0jHSZvPoW5IymJq3DyPI9bVzz8Oa3lEkxXHYx');
 
 client.search({
-  term:'Chinese',
-  location: 'los angeles'
+  term:'burgers',
+  location: 'new york'
 }).then(response => {
   console.log(response.jsonBody.businesses);
 }).catch(e => {
