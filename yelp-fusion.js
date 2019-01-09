91d419fbc59e813fca5a38eae8212e621b582f6c
pjhernandez18 <pjhernan@usc.edu>
2019-01-09 13:51:49 -0800

    added apply modal

--- yelp-fusion.js
+++ yelp-fusion.js
@@ -2,25 +2,25 @@
 
 const yelp = require('yelp-fusion');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const client = yelp.client('ezVut3lGqFJ7HXBTot9_Y8Nv2lcAwbVucOjwhk-g2iAwiyG5gnZ0NBm9U0D7X5bxAhRI4RhJ9DHVfg37r_32BRvg2Ys0jHSZvPoW5IymJq3DyPI9bVzz8Oa3lEkxXHYx');
-const app = express()
+
+
+const app = express();
 
 app.use(express.urlencoded())
-
-app.post('/submit-yelp-query', (req, res) => {
-
- const term = req.body.term
- const location = req.body.location
 
  client.search({
   term: "'" + term + "'",
   location: "'" + location + "'"
 }).then(response => {
-  res.send(response.jsonBody.businesses[0].name);
+  res.send(response.jsonBody.businesses);
 }).catch(e => {
   console.log(e);
 });
+
+app.post('/submit-yelp-query', (req, res) => {
 
  res.end()
 })
