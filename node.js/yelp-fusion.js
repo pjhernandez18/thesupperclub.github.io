00aff64c5738f10d644ca94af5f15e0d7f9ccfb4
pjhernandez18 <pjhernan@usc.edu>
2019-01-08 21:43:15 -0800

    added application form

--- node.js/yelp-fusion.js
+++ node.js/yelp-fusion.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const yelp = require('yelp-fusion');
+const express = require('express');
 
 const client = yelp.client('ezVut3lGqFJ7HXBTot9_Y8Nv2lcAwbVucOjwhk-g2iAwiyG5gnZ0NBm9U0D7X5bxAhRI4RhJ9DHVfg37r_32BRvg2Ys0jHSZvPoW5IymJq3DyPI9bVzz8Oa3lEkxXHYx');
+
 
 client.search({
   term:'burgers',
---
+++ yelp-fusion.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const yelp = require('yelp-fusion');
+const express = require('express');
+
+const client = yelp.client('ezVut3lGqFJ7HXBTot9_Y8Nv2lcAwbVucOjwhk-g2iAwiyG5gnZ0NBm9U0D7X5bxAhRI4RhJ9DHVfg37r_32BRvg2Ys0jHSZvPoW5IymJq3DyPI9bVzz8Oa3lEkxXHYx');
+const app = express()
+
+app.use(express.urlencoded())
+
+app.post('/submit-yelp-query', (req, res) => {
+
+ const term = req.body.term
+ const location = req.body.location
+
+ client.search({
+  term: "'" + term + "'",
+  location: "'" + location + "'"
+}).then(response => {
+  res.send(response.jsonBody.businesses[0].name);
+}).catch(e => {
+  console.log(e);
+});
+
+ res.end()
+})
